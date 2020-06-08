64a24d383ab9ed7fa03b2b461a25e5295d024a28
Kevinho Morales <kfmorales94@gmail.com>
2020-06-08 14:10:06 -0500

    Initial Commit

---
+++ Talk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Talk
+//
+//  Created by Kevinho Morales on 6/8/20.
+//  Copyright © 2020 Kevinho Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
